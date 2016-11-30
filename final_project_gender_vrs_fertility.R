f6a70fdc60c3f32ef59c13825094517c77edef0e
Aladago Maxwell <maxwell.aladago@gmail.com>
2016-11-30 21:41:19 +0000

    maxwell is almost done with data preparation

--- final_project_gender_vrs_fertility.R
+++ final_project_gender_vrs_fertility.R
@@ -11,6 +11,18 @@ setwd("/home/aladago/Documents/Ashesi/Fall/Data Mining/Final Project/Data_mining
 # reading the entire data
 rawData <- read.csv("final_project_data.csv", header = TRUE)
 
-#separating the two columns(gender and kids) for analysis
+# separating the two columns(gender and kids) for analysis
 processedData <- data.frame(rawData$Gender, rawData$Kids)
+
+# attempting a box plot since the response its a categorical variable vrs quantitative variable
+plot(processedData$rawData.Gender, processedData$rawData.Kids)
+
+# crosstabulating the data
+genderbyFertility <- xtabs(~processedData$rawData.Gender + processedData$rawData.Kids)
+
+# taking the proportions of the various to obtain a good representation of the data
+proportionalGenderbyFertility <- round(prop.table(genderbyFertility, 1)* 100, 2)
+
+# depicting a barplot of gender vrs number of children
+barplot(proportionalGenderbyFertility, col = c('green', 'blue'), beside = TRUE, legend =c('F', 'M'), ylab = "Proportion of parliamentarians", xlab = "Sex", main = "Plot of number of children by Gender for Ghana's MPs")
 
